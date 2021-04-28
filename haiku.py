01d22ffa4ceac7e88a1b8ee353a61383698d985a
Sushmey <sushmeywork@gmail.com>
2021-04-28 21:17:51 +0530

    added connection to mysql db and added environment variabales

--- haiku.py
+++ haiku.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, redirect
+from flask_mysqldb import MySQL
 import os
 import syllapy
-from haikuInput import Haiku
+#from dotenv import load_dotenv
+from haikuInput import Haiku, LoginForm, RegistrationForm
 app = Flask(__name__)
+
+
+#load_dotenv('.env')
+
+#SQL DB INfO
+app.config['MYSQL_USER'] = os.environ.get("USERNAME")
+app.config['MYSQL_PASSWORD'] = os.environ.get("PASSWORD")
+app.config['MYSQL_HOST'] = os.environ.get("DOMAIN")
+app.config['MYSQL_DB'] = os.environ.get("DB")
+app.config['MYSQL_CURSORCLASS'] = 'Dict'
+
 
 
 SECRET_KEY = os.urandom(32)
@@ -20,6 +33,7 @@ def haikuFormatter(haiku):
 		print(word)
 		syllables += syllapy.count(word)
 		if(syllables>= 5):
+			print(syllables)
 			if(syllables == 5):
 				word = word+os.linesep	
 			elif(syllables>=12):
@@ -32,7 +46,8 @@ def haikuFormatter(haiku):
 	return haikuFormatted	
 
 @app.route('/')
-def hello_world():
+@app.route('/home')
+def home():
     return render_template('home.html',title='Home',posts=posts)
 
 @app.route('/addhaiku',methods=['GET','POST'])
@@ -46,6 +61,25 @@ def haikuInput():
 		author = 'author'
 		posts =[{'haiku':haiku,'author':author},{}]
 	return render_template('inputPage.html',title='Add', form=form )
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	if(form.validate_on_submit()):
+		if(form.email.data=='admin@haiku.com' and form.password.data=='haikus'):
+			flash('Logged in as {}'.format('admin'),'success')
+			return redirect('home')
+		else:
+			flash('Invalid email or password','danger')
+	return render_template('login.html',title='Login', form=form)
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+	form = RegistrationForm()
+	if (form.validate_on_submit()):
+		flash('Account created for {}!'.format(form.username.data), 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html',title='Register', form=form)
 
 if(__name__=='__main__'):  #This is so that we can run it directly in debug mode
 	app.run(debug=True)    
--- haikuInput.py
+++ haikuInput.py
@@ -1,16 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Length, ValidationError, Email, EqualTo
 import syllapy
 
 
 def validate_haiku(form,haiku):
+	syllables2 = syllapy.count(haiku.data)
 	words = haiku.data.split()
 	syllables = 0
 	for word in words:
 		word = word.lower()
 		syllables+=syllapy.count(word)
-	if (syllables != 17 ):
+	if (syllables != 17 and syllables2 !=17):
 		raise ValidationError("Check the number of syllables")		
 
 
@@ -18,6 +19,21 @@ class Haiku(FlaskForm):
 	haiku = StringField('Haiku',validators=[InputRequired(), validate_haiku])
 
 	submit = SubmitField('Post!')
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Username',validators=[InputRequired(),Length(min=3,max=20)])
+	email = StringField('Email', validators=[InputRequired(),Email()])
+	password = PasswordField('Password', validators=[InputRequired(),Length(min=6)])
+	confirmPassword = PasswordField('Confirm Password',validators=[InputRequired(),EqualTo('password')])
+	submit = SubmitField('Register!')
+
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators=[InputRequired(),Email()])
+	#username = StringField('Username',validators=[InputRequired(),Length(min=3,max=20)])
+	password = PasswordField('Password', validators=[InputRequired(),Length(min=6)])
+	remember = BooleanField('Remember Me')
+	submit = SubmitField('Login!')
+
 
 
 
